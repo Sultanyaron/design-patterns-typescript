392352c9a775c19f9e4be5e235f5ebf9ced14b9c
sultanyaron <sultanyaron@gmail.com>
2020-09-19 16:57:51 +0300

    defining the sort class and bubble sort algo

--- sort-app/src/index.ts
+++ sort-app/src/index.ts
@@ -1,2 +1,5 @@
-console.log("HI THERE");
-console.log("s");
+import { Sorter } from "./Sorter";
+
+const sorter = new Sorter([5, 33333, 2, 6, 3, 3]);
+sorter.sort();
+console.log(sorter.collection);
