20f2bf2aa78b1e529b1e4b19c9276d923d5bfc0b
sultanyaron <sultanyaron@gmail.com>
2020-09-20 12:01:02 +0300

    implementing linked list and sorting it

--- sort-app/src/index.ts
+++ sort-app/src/index.ts
@@ -1,15 +1,25 @@
 import { Sorter } from "./Sorter";
 import { NumbersCollection } from "./NumbersCollection";
 import { CharactersCollection } from "./CharactersCollection";
+import { LinkedList } from "./LinkedList";
 
 const numbersCollection = new NumbersCollection([4, 2, 5, 777, 4, 3, 1, 2, 3]);
-
-const charactersCollection = new CharactersCollection("eDkegmdLdkieOdlaaakdB");
 
 const numbersSorter = new Sorter(numbersCollection);
 numbersSorter.sort();
 
+const charactersCollection = new CharactersCollection("eDkegmdLdkieOdlaaakdB");
+
 const charactersSorter = new Sorter(charactersCollection);
 charactersSorter.sort();
 
-console.log(charactersCollection.data);
+const linkedList = new LinkedList();
+linkedList.add(99);
+linkedList.add(33);
+linkedList.add(4);
+linkedList.add(2);
+
+const linkedListSorter = new Sorter(linkedList);
+linkedList.print();
+linkedListSorter.sort();
+linkedList.print();
