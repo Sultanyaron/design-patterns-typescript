dac104ff1936c49708e6ff2a2f37f94a4f2103c4
sultanyaron <sultanyaron@gmail.com>
2020-09-20 12:22:27 +0300

    making sorter an abstract class and make the data collections extends it

--- sort-app/src/index.ts
+++ sort-app/src/index.ts
@@ -4,22 +4,14 @@ import { CharactersCollection } from "./CharactersCollection";
 import { LinkedList } from "./LinkedList";
 
 const numbersCollection = new NumbersCollection([4, 2, 5, 777, 4, 3, 1, 2, 3]);
-
-const numbersSorter = new Sorter(numbersCollection);
-numbersSorter.sort();
-
 const charactersCollection = new CharactersCollection("eDkegmdLdkieOdlaaakdB");
-
-const charactersSorter = new Sorter(charactersCollection);
-charactersSorter.sort();
 
 const linkedList = new LinkedList();
 linkedList.add(99);
 linkedList.add(33);
-linkedList.add(4);
+linkedList.add(150);
 linkedList.add(2);
 
-const linkedListSorter = new Sorter(linkedList);
-linkedList.print();
-linkedListSorter.sort();
+linkedList.sort();
+
 linkedList.print();
